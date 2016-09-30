5d66141296ed9219942c1e852ae861552eba3e43
Peter <peter@ecomerc.com>
2016-09-30 10:32:35 +0200

    new version of xscreenshot

--- xscreenshot.rb
+++ xscreenshot.rb
@@ -5,9 +5,9 @@
 class Xscreenshot < Formula
   desc "A reincarnation of the screenshot gem for fastlane that works for Xamarin.Forms apps"
   homepage "https://github.com/ecomerc/xscreenshot"
-  url "https://github.com/ecomerc/xscreenshot/archive/v0.9.1.tar.gz"
-  version "0.9.1"
-  sha256 "91ab6b5d7d7c2fab56dcf3602b399381895215648ece57cde08a6e866e834d00"
+  url "https://github.com/ecomerc/xscreenshot/archive/v0.9.2.tar.gz"
+  version "0.9.2"
+  sha256 ""
 
   # depends_on "cmake" => :build
   # we require xamarin 
@@ -16,6 +16,8 @@ class Xscreenshot < Formula
 	system "/usr/local/bin/nuget", "restore", "Source/"
 	system "/usr/local/bin/xbuild", "/p:Configuration=Release", "Source/xscreenshot.sln"
 	bin.install Dir["Source/bin/Release/*"]
+	bin.install Dir["Scripts/*"]
+	opt_prefix.install Dir["Fastlane/*"]
   end
 
   test do
